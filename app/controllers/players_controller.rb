7fd7e297653741020eb1a4a6b6620dc5a899035d
asaki kusano <darumade4@gmail.com>
2020-09-29 18:05:46 +0900

    一覧に新規登録のリンク追加,翻訳情報追加,登録フォーム作成

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,13 +1,31 @@
 class PlayersController < ApplicationController
   def index
+    @players = Player.all
   end
 
   def show
+    @player = Player.find(params[:id])
   end
 
   def new
+    @player = Player.new
+  end
+
+  def create
+    @player = Player.new(player_params)
+    if @player.save
+      redirect_to player_path(@player), notice: '登録しました'
+    else
+      render :new
+    end
+
   end
 
   def edit
+  end
+
+  private
+  def player_params
+    params.require(:player).permit(:legend, :platform, :voice_chat, :play_style, :time_to_play, :favorite_weapon, :rank, :killdeath_point, :free)
   end
 end
