6b970e752e5b9bce2ea3169ffe016a8e583d78d2
asaki kusano <darumade4@gmail.com>
2020-09-29 21:17:18 +0900

    バリデーション記載,エラーメッセージ記載

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,6 @@
 class Player < ApplicationRecord
+  validates :legend, :platform, :voice_chat, :time_to_play, :rank, presence: true
+
   enum legend: { オクタン:1, クリプト:2, コースティック:3, ジブラルタル:4, バンガロール:5, パスファインダー:6, ブラッドハウンド:7, ミラージュ:8, ライフライン:9, ランパート:10, レイス:11, レヴナント:12, ローバ:13, ワットソン:14 }
   enum platform: { PS4:1, PC:2, Xbox:3 }
   enum voice_chat: { やる:1, やらない:1 }
