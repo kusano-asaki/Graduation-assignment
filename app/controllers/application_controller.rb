db62d4f2300e8da8af8f68f6911e1d0cc3575851
asaki kusano <darumade4@gmail.com>
2020-10-01 15:55:22 +0900

    protect_from_forgery の記載,ログイン中のユーザを取得,ログインの有無,ログインしていなければアプリをりようできない制限

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  include SessionsHelper
+
+  before_action :login_require
+
+  private
+  def login_require
+    redirect_to sessions_path unless current_user
+  end
+
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,15 @@
 class SessionsController < ApplicationController
+  skip_before_action :login_require, only: [:new]
   def new
   end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && User.authenticate(params[:session][:password])
+    if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       redirect_to players_path, notice: 'ログインしました'
     else
-      flash.now[:danger] = 'ログインに失敗しました'
+      flash[:danger] = 'ログインに失敗しました'
       render :new
     end
   end
