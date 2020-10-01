c78f6ab85e0e446558463ac20ee5e671198f2ff1
asaki kusano <darumade4@gmail.com>
2020-10-01 19:38:25 +0900

    playersテーブルにuser_id追加,アソシエーション定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   validates :password, presence: true, length: { minimum: 6 }
 
-
+  has_many :players
   has_secure_password
 end
