0c44679272c3ef083e6ecacc29878b9c2923ff8a
asaki kusano <darumade4@gmail.com>
2020-10-11 19:20:36 +0900

    インデント修正

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,21 +1,17 @@
-# require 'rails_helper'
-# RSpec.describe 'Player管理機能', type: :model do
-#
-#   describe 'バリデーション' do
-#
-#     it 'play_styleの入力が41文字以上ならバリデーションが通らない' do
-#       player = Player.new(play_style: 'a' * 41)
-#       expect(player).not_to be_valid
-#     end
-#
-#     it 'favorite_weaponの入力が41文字以上ならバリデーションが通らない' do
-#       player = Player.new(favorite_weapon: 'a' * 41)
-#       expect(player).not_to be_valid
-#     end
-#
-#     it 'freeの入力が251文字以上ならバリデーションが通らない' do
-#       player = Player.new(free: 'a' * 251)
-#       expect(player).not_to be_valid
-#     end
-#   end
-# end
+require 'rails_helper'
+RSpec.describe 'Player管理機能', type: :model do
+  describe 'バリデーション' do
+    it 'play_styleの入力が41文字以上ならバリデーションが通らない' do
+      player = Player.new(play_style: 'a' * 41)
+      expect(player).not_to be_valid
+    end
+    it 'favorite_weaponの入力が41文字以上ならバリデーションが通らない' do
+      player = Player.new(favorite_weapon: 'a' * 41)
+      expect(player).not_to be_valid
+    end
+    it 'freeの入力が251文字以上ならバリデーションが通らない' do
+      player = Player.new(free: 'a' * 251)
+      expect(player).not_to be_valid
+    end
+  end
+end
