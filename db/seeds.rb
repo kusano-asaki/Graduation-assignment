4425cc788bf9da7b1ad7a1c0b937c269cd3b425c
asaki kusano <darumade4@gmail.com>
2020-10-12 20:35:18 +0900

    seedデータ変更(heroku対応)

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,49 +7,49 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-20.times do |n|
-  User.create!(
-    email: "seedsample#{n + 1}@test.com",
-    name: "seed-#{n + 1}",
-    password: "password",
-    password_confirmation: "password"
-  )
-end
-
-20.times do |n|
-  Player.create!(
-    legend: 1,
-    platform: 1,
-    voice_chat: 1,
-    play_style: "enjoy",
-    time_to_play: "2020-10-09 12:00:00",
-    favorite_weapon: "マシンガン",
-    rank: 1,
-    killdeath_point: 1.5,
-    free: "よろしくお願いします",
-    user_id: 20
-  )
-end
+# 20.times do |n|
+#   User.create!(
+#     email: "seedsample#{n + 1}@test.com",
+#     name: "seed-#{n + 1}",
+#     password: "password",
+#     password_confirmation: "password"
+#   )
+# end
+#
+# 20.times do |n|
+#   Player.create!(
+#     legend: 1,
+#     platform: 1,
+#     voice_chat: 1,
+#     play_style: "enjoy",
+#     time_to_play: "2020-10-09 12:00:00",
+#     favorite_weapon: "マシンガン",
+#     rank: 1,
+#     killdeath_point: 1.5,
+#     free: "よろしくお願いします",
+#     user_id: 20
+#   )
+# end
 
 20.times do |n|
   Conversation.create!(
     sender_id: 1 + n,
-    recipient_id: 26
+    recipient_id: 22
   )
 end
 
 20.times do |n|
   Message.create!(
     body: "はじめまして!",
-    conversation_id: 25,
-    user_id: 26,
+    conversation_id: 3,
+    user_id: 22,
     read: false
   )
 end
 
 20.times do |n|
   Relationship.create!(
-    follower_id: 26,
+    follower_id: 22,
     followed_id: n + 1
   )
 end
