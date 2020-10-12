3640728fe6d6d731e638502e647605f66fad4093
asaki kusano <darumade4@gmail.com>
2020-10-12 22:08:35 +0900

    fix: config.assets.compile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap-sprockets
 //= require bootstrap/dist/js/bootstrap.min
 //= require jquery/dist/jquery.js
 //= require rails-ujs
